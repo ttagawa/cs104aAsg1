f391c342d5e747b1638891265909182b4fe236c7
Tyler <tylertagawa@gmail.com>
2015-10-22 18:33:11 -0700

    fixed the print statement, but still need the file directives part.

--- astree.cpp
+++ astree.cpp
@@ -35,9 +35,13 @@ astree* adopt2 (astree* root, astree* left, astree* right) {
 
 
 static void dump_node (FILE* outfile, astree* node) {
-   fprintf (outfile, "%p->{%s(%d) %ld:%ld.%03ld \"%s\" [",
-            node, get_yytname (node->symbol), node->symbol,
+  // fprintf (outfile, "%p->{%s(%d) %ld:%ld.%03ld \"%s\" [",
+  //          node, get_yytname (node->symbol), node->symbol,
+  //          node->filenr, node->linenr, node->offset,
+  //          node->lexinfo->c_str());
+  fprintf (outfile, "%ld %ld.%03ld %3d %-15s (%s) \n",
             node->filenr, node->linenr, node->offset,
+            node->symbol, get_yytname (node->symbol),
             node->lexinfo->c_str());
    bool need_space = false;
    for (size_t child = 0; child < node->children.size();
@@ -46,7 +50,7 @@ static void dump_node (FILE* outfile, astree* node) {
       need_space = true;
       fprintf (outfile, "%p", node->children.at(child));
    }
-   fprintf (outfile, "]}");
+//   fprintf (outfile, "]}\n");
 }
 
 static void dump_astree_rec (FILE* outfile, astree* root,
